6453b6c7f290b74bcc89071d4020b298d60b39cb
Jonata03 <jonatanery2016@gmail.com>
2024-04-19 00:25:52 -0400

    quase finalizando o de colocar o projeto em de acordo com a apostila

---
+++ ContaBancaria/src/br/com/caelum/contas/modelo/ContaCorrente.java
@@ -0,0 +1,4 @@
+package br.com.caelum.contas.modelo;
+
+public class ContaCorrente extends Conta{
+}
---
+++ ContaBancaria/src/br/com/caelum/contas/modelo/ContaPolpanca.java
@@ -0,0 +1,4 @@
+package br.com.caelum.contas.modelo;
+
+public class ContaPolpanca extends Conta{
+}
