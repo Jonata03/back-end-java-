7d11cdc3b3b110ee66584008709bf9af2ae84c45
Jonata <jonatanery2016@gmail.com>
2024-04-26 01:02:10 -0400

    .

--- ContaBancaria/src/br/com/caelum/contas/testes/TestaCollections.java
+++ ContaBancaria/src/br/com/caelum/contas/testes/TestaCollections.java
@@ -4,19 +4,18 @@ import br.com.caelum.contas.modelo.Conta;
 import br.com.caelum.contas.modelo.ContaCorrente;
 
 import java.sql.SQLOutput;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class TestaCollections {
     public static void main(String[] args) {
-        ContaCorrente c1 = new ContaCorrente();
+   /*     ContaCorrente c1 = new ContaCorrente();
         c1.deposita(100);
 
         ContaCorrente c2 = new ContaCorrente();
         c2.deposita(200);
 
         ContaCorrente c3 = new ContaCorrente();
-        c3.deposita(300);
+        c3.deposita(300);*/
 /*
         List contas = new ArrayList();
         contas.add(c1);
@@ -34,7 +33,7 @@ public class TestaCollections {
         }*/
 
 //        List <ContaCorrente> contas = new ArrayList<ContaCorrente>();
-        List <ContaCorrente> contas = new ArrayList<>();
+/*        List <ContaCorrente> contas = new ArrayList<>();
 //        perceba que como o list ja esta recebendo o tipo do objeto  nao é necessario passar novamente no ArrayList basta passar um operador em diamante ArrayList<>()
         contas.add(c1);
         contas.add(c2);
@@ -43,7 +42,32 @@ public class TestaCollections {
             ContaCorrente cc = obj;
 //            nesse caso nao é necessario fazer o casting
             System.out.println(cc.getSaldo());
-        }
+        }*/
+        /*Set<String> conjunto = new HashSet<>();
 
+        conjunto.add("item 1");
+        conjunto.add("item 2");
+        conjunto.add("item 3");
+
+        Iterator<String> i = conjunto.iterator();
+        while (i.hasNext()){
+            String palavra = i.next();
+            System.out.println(palavra);
+        }*/
+        ContaCorrente c1 = new ContaCorrente();
+        c1.deposita(1000);
+
+        ContaCorrente c2 = new ContaCorrente();
+        c2.deposita(5000);
+
+        Map<String,ContaCorrente> mapaDeContas = new HashMap<>();
+
+        mapaDeContas.put("diretor",c1);
+        mapaDeContas.put("gerente", c2);
+        mapaDeContas.put("diretor", c2);
+
+
+        ContaCorrente contaDoDiretor = mapaDeContas.get("diretor");
+        System.out.println(contaDoDiretor.getSaldo());
     }
 }
---
+++ ContaBancaria/src/br/com/caelum/contas/testes/TestaPerformance.java
@@ -0,0 +1,45 @@
+package br.com.caelum.contas.testes;
+
+import br.com.caelum.contas.modelo.ContaCorrente;
+
+import java.util.*;
+
+import br.com.caelum.contas.modelo.Conta;
+
+public class TestaPerformance {
+    public static void main(String[] args) {
+        Conta c1 = new ContaCorrente();
+        c1.deposita(100000);
+
+        Conta c2 = new ContaCorrente();
+        c2.deposita(3000);
+
+        Map mapaDeContas = new HashMap();
+        mapaDeContas.put("diretor",c1);
+        mapaDeContas.put("Gerente",c2);
+
+        Conta contaDoDiretor = (Conta) mapaDeContas.get("diretor");
+        System.out.println(contaDoDiretor.getSaldo());
+
+
+
+
+        /* System.out.println("iniciando...");
+
+        Collection<Integer> teste = new HashSet<>();
+        long inicio = System.currentTimeMillis();
+
+        int total = 3000000;
+
+        for (int i = 0; i < total; i++){
+            teste.add(i);
+        }
+        for (int i =0 ;i < total; i++){
+            teste.contains(i);
+        }
+
+        long fim = System.currentTimeMillis();
+        long tempo = fim - inicio;
+        System.out.println("Tempo Gasto: "+ tempo);*/
+    }
+}
